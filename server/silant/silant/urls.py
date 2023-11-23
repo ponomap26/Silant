b9e3656cca49bccc3c06bc17cf1e6952e4a253c9
ponomap26 <ponomap26@yandex.ru>
2023-11-24 00:43:18 +0400

    ver 1.5 model Car api

--- server/silant/silant/urls.py
+++ server/silant/silant/urls.py
@@ -1,6 +1,8 @@
 from django.urls import include, path
 from django.contrib import admin
 
+
+
 from authentication.views import TokenAuthView
 from logica.views import ModelLoadingViewSet
 
@@ -12,6 +14,6 @@ urlpatterns = [
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('json/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('token/', TokenAuthView.as_view()),
-    path('models/', ModelLoadingViewSet.as_view({'get': 'list', 'post': 'create', 'delete': 'delete'}))
-
+    path('models/', ModelLoadingViewSet.as_view({'get': 'list', 'post': 'create', })),
+    path('models/<int:pk>/', ModelLoadingViewSet.as_view({'post': 'create', 'delete': 'delete'}))
 ]
